30eddd8196c70af8a3bb5aa3f391fe2994be1d3c
JPry <JPry@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-19 02:56:12 +0000

    Create the main plugin file
    git-svn-id: http://plugins.svn.wordpress.org/remove-wp-engine-404-for-bots/trunk@877639 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ remove-wpengine-404.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Plugin Name: Remove WP Engine Bot 404
+ * Plugin URI: https://github.com/JCPry/remove-wpengine-404
+ * Description: WP Engine has a custom 404 page for bots. This plugin removes that customization.
+ * Version: 1.0
+ * Author: Jeremy Pry
+ * Author URI: http://jeremypry.com/
+ * License: GPL2
+ */
+
+// Prevent direct access to this file
+if ( ! defined( 'ABSPATH' ) ) {
+	die( "You can't do anything by accessing this file directly." );
+}
+
+// Exit if the WpeCommon class does not exist
+if ( ! class_exists( 'WpeCommon', false ) ) {
+	return;
+}
+
+// Add our hook after plugins have been loaded
+add_action( 'plugins_loaded', 'jpry_remove_wpe_404', 20 );
+
+/**
+ * Remove the custom 404 that WP Engine serves to Bots
+ *
+ * @since 1.0
+ *
+ * @see WpeCommon::wp_hook_init()
+ */
+function jpry_remove_wpe_404() {
+
+	// Technically we already know that the WpeCommon class exists, but we're checking again just to be safe
+	if ( class_exists( 'WpeCommon', false ) ) {
+
+		// Grab the WpeCommon instance
+		$wpe_common = WpeCommon::instance();
+
+		// Remove the hook from all possible places
+		remove_action( 'bp_init',		array( $wpe_common, 'is_404' ), 99999 );
+		remove_action( 'bbp_init',		array( $wpe_common, 'is_404' ), 99999 );
+		remove_action( 'template_redirect',	array( $wpe_common, 'is_404' ), 99999 );
+	}
+}
